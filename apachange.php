05136d3156173cb59ecdc99c0cf1b0311cb408bd
Carlos Silva <csilva@cntlog.com.br>
2018-10-03 14:31:50 -0300

    Corrective modifications in apachange.php and setup.php

--- apachange.php
+++ apachange.php
@@ -2,81 +2,100 @@
 
 $num=shell_exec('tput cols');
 
-for($i=0;$i<$num;++$i) {
+for ($i=0;$i<$num;++$i) {
   echo"_";
 }
 
-for($i=0;$i<$num;++$i) {
+for ($i=0;$i<$num;++$i) {
   echo" ";
 }
 
-for($i=0;$i<$num;++$i) {
+for ($i=0;$i<$num;++$i) {
   echo" ";
 }
 
 echo" Apachange \n";
 
-for($i=0;$i<$num;++$i) {
+for ($i=0;$i<$num;++$i) {
   echo"_";
 }
 
-if(isset($argv[1])) {
+if (isset($argv[1])) {
   $argument1=$argv[1];
 } else {
   $argument1='a';
 }
 
-if($argument1=='-c') {
-  $path=system('pwd');
-  echo"Setting ".$path." as new Root for Apache \n";
-} elseif($argument1=='-r') {
+if ($argument1 == '-c') {
+  $path = system('pwd');
+  echo "Setting " . $path . " as new Root for Apache \n";
+} elseif ($argument1 == '-r') {
+  if (!file_exists('/etc/apache2/sites-available/000-default.conf')) {
+    echo "'/etc/apache2/sites-available/000-default.conf' not found.\n";
+    echo "You need to execute 'php setup.php' at least one time before to use this command.\n";
+    echo"Failed to restore the default Root for Apache settings\n";
+    die();
+  }
+  if (!file_exists('/etc/apache2/apache2.conf')) {
+    echo "'/etc/apache2/apache2.conf' not found.\n";
+    echo "You need to execute 'php setup.php' at least one time before to use this command.\n";
+    echo"Failed to restore the default Root for Apache settings\n";
+    die();
+  }
   exec("sudo rm -f /etc/apache2/sites-available/000-default.conf");
   exec("sudo mv /etc/apache2/sites-available/000-default.conf.bckp /etc/apache2/sites-available/000-default.conf");
+  exec("sudo chmod 777 /etc/apache2/sites-available/000-default.conf");
   exec("sudo rm -f /etc/apache2/apache2.conf");
   exec("sudo mv /etc/apache2/apache2.conf.bckp /etc/apache2/apache2.conf");
-  shell_exec('sudo service apache2 restart');
-  echo"Restored the default Root for Apache settings \n";
+  exec("sudo chmod 777 /etc/apache2/apache2.conf");
+  shell_exec('sudo service apache2 restart &');
+  echo"Restored the default Root for Apache settings\n";
   die();
 } else {
-    $path=readline("Enter the directory to change Root for Apache  \n");
+  $invalid = false;
+  do {
+    $path = readline("Enter the directory to change Root for Apache  \n");
+    if ($invalid) {
+      echo "Invalid path! Single dot is not allowed.\n";
+    }
+  } while ($invalid = ($path == '.'));
 }
 
-if($path=='~') {
+if ($path == '~') {
   echo "Username: \n";
-  $user=system('echo $USER');
-  $path='/home/'.$user;
+  $user = system('echo $USER');
+  $path = "//home//" . $user;
 }
 
-$str='No Such Path';
-$op=shell_exec("if [ ! -d '".$path."' ]; then echo '".$str."'; fi");
+$str = 'No Such Path';
+$op = shell_exec("if [ ! -d '".$path."' ]; then echo '".$str."'; fi");
 echo $op;
 
-if (strcmp($op,"No Such Path\n")==0) {
+if (strcmp($op,"No Such Path\n") == 0) {
   die();
 } else {
-  $apache='//etc/apache2/sites-available/000-default.conf';
-  $file=file_get_contents($apache);
-  $pattern='/DocumentRoot/';
+  $apache = "//etc//apache2//sites-available//000-default.conf";
+  $file = file_get_contents($apache);
+  $pattern = "//DocumentRoot//";
   //preg_match($pattern,$file,$matches,PREG_OFFSET_CAPTURE);
-  $patt='DocumentRoot';
-  $pos=strpos($file,$patt);
-  $pathy=$pos+13;
-  for ($y=0;$y<50;++$y) {
-      $string=$file[$pathy+$y];
-      if (strstr($file[$pathy+$y], PHP_EOL)) {
-        $posi=$pathy+$y;
+  $patt = "DocumentRoot";
+  $pos = strpos($file,$patt);
+  $pathy = $pos+13;
+  for ($y = 0; $y < 50; $y++) {
+      $string = $file[$pathy + $y];
+      if (strstr($file[$pathy + $y], PHP_EOL)) {
+        $posi = $pathy + $y;
       }
   }
   echo "Current root for Apache: \n";
-  $string='';
-  for ($i=$pathy;$i<$posi;++$i) {
+  $string = '';
+  for ($i = $pathy; $i < $posi; $i++) {
     echo $file[$i];
-    $string.=$file[$i];
+    $string .= $file[$i];
   }
-  $file1=str_replace($string,$path,$file);
+  $file1 = str_replace($string, $path, $file);
   file_put_contents($apache,$file1);
   echo "\n";
-  $cmd=shell_exec('sudo service apache2 restart');
+  $cmd = shell_exec('sudo service apache2 restart &');
   echo "\n";
 }
-?>
--- setup.php
+++ setup.php
@@ -7,9 +7,8 @@ exec("sudo chmod 777 /etc/apache2/sites-available/000-default.conf");
 exec("sudo cp -n /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bckp");
 exec("sudo chmod 777 /etc/apache2/apache2.conf");
 exec("sudo cp apachange /usr/bin");
-exec("sudo \cp  apachange.php /usr/bin");
+exec("sudo cp apachange.php /usr/bin");
 $apache=file_get_contents('apache.conf');
 $file='/etc/apache2/apache2.conf';
 file_put_contents($file,$apache);
 echo "You are Ready to Use the Command\n";
-?>
